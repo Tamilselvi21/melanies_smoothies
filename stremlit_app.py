488fa1766872f450176b9cd824ea2067ba610c9a
Tamilselvi21 <56407748+Tamilselvi21@users.noreply.github.com>
2024-06-24 19:34:40 +0530

    Update d fruity vice stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -46,3 +46,7 @@ if ingredients_list:
        st.success('Your Smoothie is ordered!',icon="✅")
 
 #st.write("You selected:", ingredients_list)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
